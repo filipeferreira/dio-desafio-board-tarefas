8203ff076e4624802bab83c4141d31805feccfa6
Filipe Ferreira <filipef@ciasc.sc.gov.br>
2025-03-29 08:53:42 -0300

    creating the flyway migrations

---
+++ src/main/resources/db/migration/V20250329_02__create_table_boards_columns.sql
@@ -0,0 +1,9 @@
+CREATE TABLE BOARDS_COLUMNS(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    "order" INT NOT NULL,
+    kind VARCHAR(7) NOT NULL,
+    board_id BIGINT NOT NULL,
+    CONSTRAINT boards__boards_columns_fk FOREIGN KEY (board_id) REFERENCES BOARDS(id) ON DELETE CASCADE,
+    CONSTRAINT id_order_uk UNIQUE (board_id, "order")
+);
